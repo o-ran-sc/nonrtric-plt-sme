8edfce3d26a7dffeb6de2659c0a24ca578715d6f
ychacon <yennifer.chacon@est.tech>
2023-02-08 09:41:34 +0100

    Changes in implementation of Security API - get Token
    Issue-ID: NONRTRIC-836
    Signed-off-by: ychacon <yennifer.chacon@est.tech>
    Change-Id: I5eb350a63582c6e24651dd89f392dab0a9db1d8b

--- capifcore/internal/securityservice/security.go
+++ capifcore/internal/securityservice/security.go
@@ -23,7 +23,9 @@ package security
 import (
 	"net/http"
 	"strings"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 
 	"oransc.org/nonrtric/capifcore/internal/common29122"
@@ -33,6 +35,15 @@ import (
 	"oransc.org/nonrtric/capifcore/internal/providermanagement"
 	"oransc.org/nonrtric/capifcore/internal/publishservice"
 )
+
+type AccessTokenReq struct {
+	ClientId     string                              `json:"client_id" form:"client_id"`
+	ClientSecret *string                             `json:"client_secret,omitempty" form:"client_secret"`
+	GrantType    securityapi.AccessTokenReqGrantType `json:"grant_type" form:"grant_type"`
+	Scope        *string                             `json:"scope,omitempty" form:"scope"`
+}
+
+var jwtKey = "my-secret-key"
 
 type Security struct {
 	serviceRegister providermanagement.ServiceRegister
@@ -49,34 +60,77 @@ func NewSecurity(serviceRegister providermanagement.ServiceRegister, publishRegi
 }
 
 func (s *Security) PostSecuritiesSecurityIdToken(ctx echo.Context, securityId string) error {
-	clientId := ctx.FormValue("client_id")
-	clientSecret := ctx.FormValue("client_secret")
-	scope := ctx.FormValue("scope")
+	var accessTokenReq AccessTokenReq
 
-	if !s.invokerRegister.IsInvokerRegistered(clientId) {
-		return sendCoreError(ctx, http.StatusBadRequest, "Invoker not registered")
+	if err := ctx.Bind(&accessTokenReq); err != nil {
+		return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorInvalidRequest, "Invalid request")
 	}
-	if !s.invokerRegister.VerifyInvokerSecret(clientId, clientSecret) {
-		return sendCoreError(ctx, http.StatusBadRequest, "Invoker secret not valid")
+
+	if accessTokenReq.GrantType != securityapi.AccessTokenReqGrantTypeClientCredentials {
+		return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorUnsupportedGrantType, "Invalid value for grant_type")
 	}
-	if scope != "" {
-		scopeData := strings.Split(strings.Split(scope, "#")[1], ":")
-		if !s.serviceRegister.IsFunctionRegistered(scopeData[0]) {
-			return sendCoreError(ctx, http.StatusBadRequest, "Function not registered")
+
+	if !s.invokerRegister.IsInvokerRegistered(accessTokenReq.ClientId) {
+		return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorInvalidClient, "Invoker not registered")
+	}
+
+	if accessTokenReq.ClientSecret != nil {
+		if !s.invokerRegister.VerifyInvokerSecret(accessTokenReq.ClientId, *accessTokenReq.ClientSecret) {
+			return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorUnauthorizedClient, "Invoker secret not valid")
 		}
-		if !s.publishRegister.IsAPIPublished(scopeData[0], scopeData[1]) {
-			return sendCoreError(ctx, http.StatusBadRequest, "API not published")
+	}
+	//3gpp#aefId1:apiName1,apiName2,…apiNameX;aefId2:apiName1,apiName2,…apiNameY;…aefIdN:apiName1,apiName2,…apiNameZ
+	if accessTokenReq.Scope != nil {
+		scope := strings.Split(*accessTokenReq.Scope, "#")
+		if len(scope) < 2 {
+			return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorInvalidScope, "Malformed scope")
 		}
+		if scope[0] != "3gpp" {
+			return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorInvalidScope, "Scope should start with 3gpp")
+		}
+
+		aefList := strings.Split(scope[1], ";")
+		for _, aef := range aefList {
+			apiList := strings.Split(aef, ":")
+			if len(apiList) < 2 {
+				return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorInvalidScope, "Malformed scope")
+			}
+			if !s.serviceRegister.IsFunctionRegistered(apiList[0]) {
+				return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorInvalidScope, "AEF Function not registered")
+			}
+			for _, api := range strings.Split(apiList[1], ",") {
+				if !s.publishRegister.IsAPIPublished(apiList[0], api) {
+					return sendAccessTokenError(ctx, http.StatusBadRequest, securityapi.AccessTokenErrErrorInvalidScope, "API not published")
+				}
+			}
+		}
+	}
+
+	expirationTime := time.Now().Add(time.Hour).Unix()
+
+	claims := &jwt.MapClaims{
+		"iss": accessTokenReq.ClientId,
+		"exp": expirationTime,
+		"data": map[string]interface{}{
+			"scope": accessTokenReq.Scope,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(jwtKey))
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		return err
 	}
 
 	accessTokenResp := securityapi.AccessTokenRsp{
-		AccessToken: "asdadfsrt dsr t5",
-		ExpiresIn:   0,
-		Scope:       &scope,
+		AccessToken: tokenString,
+		ExpiresIn:   common29122.DurationSec(expirationTime),
+		Scope:       accessTokenReq.Scope,
 		TokenType:   "Bearer",
 	}
 
-	err := ctx.JSON(http.StatusCreated, accessTokenResp)
+	err = ctx.JSON(http.StatusCreated, accessTokenResp)
 	if err != nil {
 		// Something really bad happened, tell Echo that our handler failed
 		return err
@@ -105,11 +159,10 @@ func (s *Security) PostTrustedInvokersApiInvokerIdUpdate(ctx echo.Context, apiIn
 	return ctx.NoContent(http.StatusNotImplemented)
 }
 
-func sendCoreError(ctx echo.Context, code int, message string) error {
-	pd := common29122.ProblemDetails{
-		Cause:  &message,
-		Status: &code,
+func sendAccessTokenError(ctx echo.Context, code int, err securityapi.AccessTokenErrError, message string) error {
+	accessTokenErr := securityapi.AccessTokenErr{
+		Error:            err,
+		ErrorDescription: &message,
 	}
-	err := ctx.JSON(code, pd)
-	return err
+	return ctx.JSON(code, accessTokenErr)
 }
