e79a30674e902ba95a3bf7a7c48a4fd29dec0a9a
DenisGNoonan <denis.noonan@est.tech>
2023-11-16 15:58:51 +0000

    NONRTRIC-946:Remove hard-coded kong domain
    Issue-ID: NONRTRIC-946
    Signed-off-by: DenisGNoonan <denis.noonan@est.tech>
    Change-Id: Ib298370047713d9e51f3279a92fd9077658eec0b

--- capifcore/internal/discoverservice/discoverservice.go
+++ capifcore/internal/discoverservice/discoverservice.go
@@ -48,18 +48,12 @@ func (ds *DiscoverService) GetAllServiceAPIs(ctx echo.Context, params discoverap
 	if allApis == nil {
 		return sendCoreError(ctx, http.StatusNotFound, fmt.Sprintf("Invoker %s not registered", params.ApiInvokerId))
 	}
+
 	filteredApis := []publishapi.ServiceAPIDescription{}
-	gatewayDomain := "r1-expo-func-aef"
 	for _, api := range *allApis {
-		if !matchesFilter(api, params) {
-			continue
+		if matchesFilter(api, params) {
+			filteredApis = append(filteredApis, api)
 		}
-		profiles := *api.AefProfiles
-		for i, profile := range profiles {
-			profile.DomainName = &gatewayDomain // Hardcoded for now. Should be provided through some other mechanism.
-			profiles[i] = profile
-		}
-		filteredApis = append(filteredApis, api)
 	}
 	discoveredApis := discoverapi.DiscoveredAPIs{
 		ServiceAPIDescriptions: &filteredApis,
