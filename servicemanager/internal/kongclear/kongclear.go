f2adad008c37b48e3386dfb7cf3949d7175f96b0
DenisGNoonan <denis.noonan@est.tech>
2024-03-21 09:29:57 +0000

    NONRTRIC-944: ServiceManager - Add kongclearup to Docker image
    Issue-ID: NONRTRIC-944
    Change-Id: I9c959d6930070acde22d59d3df3248ba125f8f68
    Signed-off-by: DenisGNoonan <denis.noonan@est.tech>

--- servicemanager/internal/kongclear/kongclear.go
+++ servicemanager/internal/kongclear/kongclear.go
@@ -24,16 +24,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
-	log "github.com/sirupsen/logrus"
 	resty "github.com/go-resty/resty/v2"
+	log "github.com/sirupsen/logrus"
 )
 
 // Service represents the structure for Kong service creation
 type KongService struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	URL  string `json:"url"`
+	ID   string   `json:"id"`
+	Name string   `json:"name"`
+	URL  string   `json:"url"`
+	Tags []string `json:"tags"`
 }
 
 type KongServiceResponse struct {
@@ -50,6 +52,7 @@ type KongRoute struct {
 	Paths     []string `json:"paths"`
 	Service   Service  `json:"service"`
 	StripPath bool     `json:"strip_path"`
+	Tags      []string `json:"tags"`
 }
 
 type RouteResponse struct {
@@ -61,78 +64,58 @@ type Service struct {
 }
 
 func KongClear(myEnv map[string]string, myPorts map[string]int) error {
-	log.Info("attempting to delete all Kong routes and services")
+	log.Info("delete only ServiceManager Kong routes and services")
 
 	kongAdminApiUrl := fmt.Sprintf("%s://%s:%d/", myEnv["KONG_PROTOCOL"], myEnv["KONG_IPV4"], myPorts["KONG_CONTROL_PLANE_PORT"])
 
-	err := deleteAllRoutes(kongAdminApiUrl)
+	err := deleteRoutes(kongAdminApiUrl)
 	if err != nil {
 		log.Fatalf("error deleting routes %v", err)
 		return err
 	}
 
-	err = deleteAllServices(kongAdminApiUrl)
+	err = deleteServices(kongAdminApiUrl)
 	if err != nil {
 		log.Fatalf("error deleting services %v", err)
 		return err
 	}
 
-	log.Info("all Kong routes and services deleted successfully")
+	log.Info("finished deleting only ServiceManger Kong routes and services")
 	return err
 }
 
-
-func deleteAllRoutes(kongAdminApiUrl string) error {
+func deleteRoutes(kongAdminApiUrl string) error {
 	routes, err := listRoutes(kongAdminApiUrl)
 	if err != nil {
 		return err
 	}
 
 	for _, route := range routes {
-		if err := deleteRoute(kongAdminApiUrl, route.ID); err != nil {
-			return err
+		if areServiceManagerTags(route.Tags) {
+			if err := deleteRoute(kongAdminApiUrl, route.ID); err != nil {
+				return err
+			}
 		}
 	}
 
 	return nil
 }
 
-func deleteAllServices(kongAdminApiUrl string) error {
+func deleteServices(kongAdminApiUrl string) error {
 	services, err := listServices(kongAdminApiUrl)
 	if err != nil {
 		return err
 	}
 
 	for _, service := range services {
-		if err := deleteService(kongAdminApiUrl, service.ID); err != nil {
-			return err
+		if areServiceManagerTags(service.Tags) {
+			if err := deleteService(kongAdminApiUrl, service.ID); err != nil {
+				return err
+			}
 		}
 	}
 
 	return nil
-}
-
-func listServices(kongAdminApiUrl string) ([]KongService, error) {
-	client := resty.New()
-	resp, err := client.R().Get(kongAdminApiUrl + "services")
-
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode() != http.StatusOK {
-		err := fmt.Errorf("failed to list services, status code %d", resp.StatusCode())
-		return nil, err
-	}
-
-	var serviceResponse ServiceResponse
-	err = json.Unmarshal(resp.Body(), &serviceResponse)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Infof("kong services %v", serviceResponse.Data)
-	return serviceResponse.Data, nil
 }
 
 func listRoutes(kongAdminApiUrl string) ([]KongRoute, error) {
@@ -159,25 +142,68 @@ func listRoutes(kongAdminApiUrl string) ([]KongRoute, error) {
 	return routeResponse.Data, nil
 }
 
-func deleteService(kongAdminApiUrl string, serviceID string) error {
-	log.Tracef("entering deleteService service ID %v", serviceID)
+func listServices(kongAdminApiUrl string) ([]KongService, error) {
 	client := resty.New()
-	resp, err := client.R().Delete(kongAdminApiUrl + "services/" + serviceID)
+	resp, err := client.R().Get(kongAdminApiUrl + "services")
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if resp.StatusCode() != http.StatusNoContent {
-		err := fmt.Errorf("failed to delete service %s, status code %d", serviceID, resp.StatusCode())
-		return err
+	if resp.StatusCode() != http.StatusOK {
+		err := fmt.Errorf("failed to list services, status code %d", resp.StatusCode())
+		return nil, err
 	}
 
-	return nil
+	var serviceResponse ServiceResponse
+	err = json.Unmarshal(resp.Body(), &serviceResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Infof("kong services %v", serviceResponse.Data)
+	return serviceResponse.Data, nil
+}
+
+func areServiceManagerTags(tags []string) bool {
+	tagMap := make(map[string]string)
+
+	for _, tag := range tags {
+		log.Debugf("found tag %s", tag)
+		tagSlice := strings.Split(tag, ":")
+		log.Debugf("tag slice %v", tagSlice)
+		if (len(tagSlice) > 0) && (tagSlice[0] != "") {
+			if (len(tagSlice) > 1) {
+				tagMap[tagSlice[0]] = tagSlice[1]
+			} else {
+				tagMap[tagSlice[0]] = ""
+			}
+		}
+	}
+
+	if tagMap["apfId"] == "" {
+		log.Debug("did NOT find apfId")
+		return false
+	}
+	log.Debugf("found valid apfId %s", tagMap["apfId"])
+
+	if tagMap["aefId"] == "" {
+		log.Debug("did NOT find aefId")
+		return false
+	}
+	log.Debugf("found valid aefId %s", tagMap["aefId"])
+
+	if tagMap["apiId"] == "" {
+		log.Debug("did NOT find apiId")
+		return false
+	}
+	log.Debugf("found valid apiId %s", tagMap["apiId"])
+
+	return true
 }
 
 func deleteRoute(kongAdminApiUrl string, routeID string) error {
-	log.Infof("kong route id %v", routeID)
+	log.Infof("delete kong route id %s", routeID)
 	client := resty.New()
 	resp, err := client.R().Delete(kongAdminApiUrl + "routes/" + routeID)
 
@@ -187,6 +213,23 @@ func deleteRoute(kongAdminApiUrl string, routeID string) error {
 
 	if resp.StatusCode() != http.StatusNoContent {
 		err := fmt.Errorf("failed to delete route %s, status code %d", routeID, resp.StatusCode())
+		return err
+	}
+
+	return nil
+}
+
+func deleteService(kongAdminApiUrl string, serviceID string) error {
+	log.Infof("delete kong service id %s", serviceID)
+	client := resty.New()
+	resp, err := client.R().Delete(kongAdminApiUrl + "services/" + serviceID)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != http.StatusNoContent {
+		err := fmt.Errorf("failed to delete service %s, status code %d", serviceID, resp.StatusCode())
 		return err
 	}
 
