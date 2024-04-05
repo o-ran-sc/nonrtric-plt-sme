5493b0faf67fc5b58b575880db528eb2b663d45a
DenisGNoonan <denis.noonan@est.tech>
2024-04-05 16:35:26 +0100

    NONRTRIC-946: Servicemanager - mock kong and capif as library
    Issue-ID: NONRTRIC-946
    Change-Id: I0633d6b117a22b0f788f104670471f07104f0375
    Signed-off-by: DenisGNoonan <denis.noonan@est.tech>

--- capifcore/go.mod
+++ capifcore/go.mod
@@ -8,20 +8,13 @@ require (
 	github.com/labstack/echo/v4 v4.10.2
 	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.1
-	github.com/swaggo/swag v1.8.12
 	gopkg.in/yaml.v2 v2.4.0
 	helm.sh/helm/v3 v3.9.0
 	k8s.io/cli-runtime v0.25.3-rc.0
 	k8s.io/client-go v0.25.3-rc.0
 )
 
-require (
-	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/go-openapi/spec v0.20.8 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/swaggo/files/v2 v2.0.0 // indirect
-	golang.org/x/tools v0.8.0 // indirect
-)
+require github.com/pkg/errors v0.9.1 // indirect
 
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
@@ -113,7 +106,6 @@ require (
 	github.com/spf13/cobra v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/swaggo/echo-swagger v1.4.0
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
--- servicemanager/internal/publishserviceapi/typeupdate.go
+++ servicemanager/internal/publishserviceapi/typeupdate.go
@@ -145,7 +145,8 @@ func (sd *ServiceAPIDescription) createKongRoute(
 	if resp.StatusCode() == http.StatusCreated {
 		log.Infof("kong route %s created successfully", routeName)
 	} else {
-		err = fmt.Errorf("the Kong service already exists. Status code: %d", resp.StatusCode())
+		log.Debugf("kongRoutesURL %s", kongRoutesURL)
+		err = fmt.Errorf("error creating Kong route. Status code: %d", resp.StatusCode())
 		log.Error(err.Error())
 		log.Errorf("response body: %s", resp.Body())
 		return resp.StatusCode(), err
@@ -237,7 +238,7 @@ func (sd *ServiceAPIDescription) createKongService(kongControlPlaneURL string, k
 	} else if resp.StatusCode() == http.StatusConflict {
 		log.Errorf("kong service already exists. Status code: %d", resp.StatusCode())
 		err = fmt.Errorf("service with identical apiName is already published") // for compatibilty with Capif error message on a duplicate service
-		statusCode = http.StatusForbidden // for compatibilty with the spec, TS29222_CAPIF_Publish_Service_API
+		statusCode = http.StatusForbidden                                       // for compatibilty with the spec, TS29222_CAPIF_Publish_Service_API
 	} else {
 		err = fmt.Errorf("error creating Kong service. Status code: %d", resp.StatusCode())
 	}
@@ -350,6 +351,7 @@ func (sd *ServiceAPIDescription) deleteKongRoute(kongControlPlaneURL string, cli
 	if resp.StatusCode() == http.StatusNoContent {
 		log.Infof("kong route %s deleted successfully", routeName)
 	} else {
+		log.Debugf("kongRoutesURL: %s", kongRoutesURL)
 		log.Errorf("error deleting Kong route. Status code: %d", resp.StatusCode())
 		log.Errorf("response body: %s", resp.Body())
 		return resp.StatusCode(), err
@@ -386,6 +388,7 @@ func (sd *ServiceAPIDescription) deleteKongService(kongControlPlaneURL string, s
 	if resp.StatusCode() == http.StatusNoContent {
 		log.Infof("kong service %s deleted successfully", serviceName)
 	} else {
+		log.Debugf("kongServicesURL: %s", kongServicesURL)
 		log.Errorf("deleting Kong service, status code: %d", resp.StatusCode())
 		log.Errorf("response body: %s", resp.Body())
 	}
